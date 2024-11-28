43df11ab0033897715394f64e4c4169802f6c9bd
nazarnass <nazarychevaanastacia@yandex.ru>
2024-11-28 22:02:07 +0300

    7_5_Вызов_метода_с_возвращаемым_типом

---
+++ src/main/java/ru/oop/DummyDic.java
@@ -0,0 +1,12 @@
+package ru.oop;
+
+public class DummyDic {
+    public String engToRus(String eng) {
+        return "Неизвестное слово " + eng;
+    }
+
+    public static void main(String[] args) {
+        DummyDic dummyDic = new DummyDic();
+        System.out.println(dummyDic.engToRus("word"));
+    }
+}
