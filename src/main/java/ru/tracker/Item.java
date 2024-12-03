74fa9d7f5f6c446029dce33759c58e65054cf958
nazarnass <nazarychevaanastacia@yandex.ru>
2024-12-04 02:27:47 +0300

    7.9 Перегрузка конструктора

--- src/main/java/ru/tracker/Item.java
+++ src/main/java/ru/tracker/Item.java
@@ -4,6 +4,32 @@ public class Item {
     private int id;
     private String name;
 
+    public Item() {
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void printInfo() {
+        System.out.println("Id: " + id);
+        System.out.println("Name: " + name);
+    }
+
+    public static void main(String[] args) {
+        Item item = new Item();
+        item.printInfo();
+        Item itemTwo = new Item("Nastya");
+        itemTwo.printInfo();
+        Item itemTree = new Item(7777, "Nastya Nazarycheva");
+        itemTree.printInfo();
+    }
+
     public int getId() {
         return id;
     }
