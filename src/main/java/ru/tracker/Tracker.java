56b58899f91c287518083a5b87083905ee693ddd
nazarnass <nazarychevaanastacia@yandex.ru>
2025-01-11 21:43:08 +0300

    9.8 Метод замены заявки. Tracker.replace

--- src/main/java/ru/tracker/Tracker.java
+++ src/main/java/ru/tracker/Tracker.java
@@ -14,11 +14,7 @@ public class Tracker {
     }
 
     public Item[] findAll() {
-        Item[] rsl = new Item[size];
-        for (int i = 0; i < size; i++) {
-            rsl[i] = items[i];
-        }
-        return rsl;
+        return Arrays.copyOf(items, size);
     }
 
     public Item[] findByName(String key) {
@@ -27,8 +23,7 @@ public class Tracker {
         for (int i = 0; i < size; i++) {
             Item item = items[i];
             if (item.getName().equals(key)) {
-                result[count] = item;
-                count++;
+                result[count++] = item;
             }
         }
         return Arrays.copyOf(result, count);
@@ -36,10 +31,27 @@ public class Tracker {
 
     public Item findById(int id) {
         Item rsl = null;
+        if (indexOf(id) != -1) {
+            rsl = items[indexOf(id)];
+        }
+        return rsl;
+    }
+
+    public boolean replace(int id, Item item) {
+        if (indexOf(id) != -1) {
+            item.setId(id);
+            items[indexOf(id)] = item;
+            return true;
+        }
+        return false;
+    }
+
+    private int indexOf(int id) {
+        int rsl = -1;
         for (int index = 0; index < size; index++) {
             Item item = items[index];
             if (item.getId() == id) {
-                rsl = item;
+                rsl = index;
                 break;
             }
         }
