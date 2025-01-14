2685891eb2297ef3c883f98a4ced362d9ce629c4
nazarnass <nazarychevaanastacia@yandex.ru>
2025-01-14 15:45:01 +0300

    9.10 Что такое валидация

--- src/main/java/ru/tracker/Tracker.java
+++ src/main/java/ru/tracker/Tracker.java
@@ -58,8 +58,7 @@ public class Tracker {
 
     public void delete(int id) {
         int index = indexOf(id);
-        boolean rsl = index != -1;
-        if (rsl) {
+        if (index != -1) {
             System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
