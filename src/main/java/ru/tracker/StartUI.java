a4469a049a53cd779487ffa802d4d16b8f358a88
nazarnass <nazarychevaanastacia@yandex.ru>
2025-01-29 16:46:30 +0300

    10.7 StartUI. Редактирование заявки

--- src/main/java/ru/tracker/StartUI.java
+++ src/main/java/ru/tracker/StartUI.java
@@ -28,15 +28,15 @@ public class StartUI {
                 }
             } else if (select == 2) {
                 System.out.println("=== Редактирование заявки ===");
-                System.out.print("Введите id заявки: ");
+                System.out.print("Введите id: ");
                 int id = Integer.parseInt(scanner.nextLine());
                 System.out.print("Введите имя: ");
                 String name = scanner.nextLine();
                 Item item = new Item(name);
                 if (tracker.replace(id, item)) {
-                    System.out.println("Заявка изменена");
+                    System.out.println("Заявка изменена успешно.");
                 } else {
-                    System.out.println("Ошибка! Введите правильный id");
+                    System.out.println("Ошибка замены заявки.");
                 }
             } else if (select == 3) {
                 System.out.println("=== Удаление заявки ===");
