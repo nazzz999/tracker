0939a61b7be89d653d1bc2f4425f3cd4e72f819e
nazarnass <nazarychevaanastacia@yandex.ru>
2025-01-31 18:05:51 +0300

    10.13 Input

---
+++ src/main/java/ru/poly/Bus.java
@@ -0,0 +1,19 @@
+package ru.poly;
+
+public class Bus implements Transport {
+
+    @Override
+    public void drive() {
+
+    }
+
+    @Override
+    public void passengers(int passengers) {
+
+    }
+
+    @Override
+    public int refuel(int fuel) {
+        return 0;
+    }
+}
---
+++ src/main/java/ru/poly/Transport.java
@@ -0,0 +1,9 @@
+package ru.poly;
+
+public interface Transport {
+    void drive();
+
+    void passengers(int passengers);
+
+    int refuel(int fuel);
+}
