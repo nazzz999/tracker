f03180fa049caf6c7936428614e1aa8d4afbba04
nazarnass <nazarychevaanastacia@yandex.ru>
2025-02-06 00:50:58 +0300

    10_23_Реализация_меню_за_счет_шаблона_стратегия

---
+++ src/main/java/ru/tracker/DeleteAction.java
@@ -0,0 +1,20 @@
+package ru.tracker;
+
+public class DeleteAction implements UserAction {
+    @Override
+    public String name() {
+        return "Удалить заявку";
+    }
+
+    @Override
+    public boolean execute(Input input, Tracker tracker) {
+        System.out.println("=== Удаление заявки ===");
+        int id = input.askInt("Введите id: ");
+        if (tracker.delete(id)) {
+            System.out.println("Заявка удалена успешно.");
+        } else {
+            System.out.println("Ошибка удаления заявки.");
+        }
+        return true;
+    }
+}
