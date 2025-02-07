e35ba44106d23ef144df1d4f8f958b79d9dcafa6
nazarnass <nazarychevaanastacia@yandex.ru>
2025-02-07 13:44:27 +0300

    10.25 Зависимость от System.out

--- src/main/java/ru/tracker/DeleteAction.java
+++ src/main/java/ru/tracker/DeleteAction.java
@@ -1,6 +1,12 @@
 package ru.tracker;
 
 public class DeleteAction implements UserAction {
+    private final Output output;
+
+    public DeleteAction(Output output) {
+        this.output = output;
+    }
+
     @Override
     public String name() {
         return "Удалить заявку";
@@ -8,12 +14,12 @@ public class DeleteAction implements UserAction {
 
     @Override
     public boolean execute(Input input, Tracker tracker) {
-        System.out.println("=== Удаление заявки ===");
+        output.println("=== Удаление заявки ===");
         int id = input.askInt("Введите id: ");
         if (tracker.delete(id)) {
-            System.out.println("Заявка удалена успешно.");
+            output.println("Заявка удалена успешно.");
         } else {
-            System.out.println("Ошибка удаления заявки.");
+            output.println("Ошибка удаления заявки.");
         }
         return true;
     }
--- src/test/java/ru/tracker/StartUITest.java
+++ src/test/java/ru/tracker/StartUITest.java
@@ -7,46 +7,67 @@ class StartUITest {
 
     @Test
     void whenCreateItem() {
+        Output output = new MockOutput();
         Input input = new MockInput(
                 new String[] {"0", "Item name", "1"}
         );
         Tracker tracker = new Tracker();
         UserAction[] actions = {
-                new CreateAction(),
-                new ExitAction()
+                new CreateAction(output),
+                new ExitAction(output)
         };
-        new StartUI().init(input, tracker, actions);
+        new StartUI(output).init(input, tracker, actions);
         assertThat(tracker.findAll()[0].getName()).isEqualTo("Item name");
     }
 
     @Test
     void whenReplaceItem() {
+        Output output = new MockOutput();
         Input input = new MockInput(
                 new String[] {"0", "1", "Item name", "1"}
         );
         Tracker tracker = new Tracker();
         tracker.add(new Item("Nastya"));
         UserAction[] actions = {
-                new ReplaceAction(),
-                new ExitAction()
+                new ReplaceAction(output),
+                new ExitAction(output)
         };
-        new StartUI().init(input, tracker, actions);
+        new StartUI(output).init(input, tracker, actions);
         assertThat(tracker.findAll()[0].getName()).isEqualTo("Item name");
         assertThat(tracker.findAll()[0].getId()).isEqualTo(1);
     }
 
     @Test
     void whenDeleteItem() {
+        Output output = new MockOutput();
         Input input = new MockInput(
                 new String[] {"0", "1", "1"}
         );
         Tracker tracker = new Tracker();
         tracker.add(new Item("Nastya"));
         UserAction[] actions = {
-                new DeleteAction(),
-                new ExitAction()
+                new DeleteAction(output),
+                new ExitAction(output)
         };
-        new StartUI().init(input, tracker, actions);
+        new StartUI(output).init(input, tracker, actions);
         assertThat(tracker.findById(1)).isNull();
+    }
+
+    @Test
+    void whenExit() {
+        Output output = new MockOutput();
+        Input input = new MockInput(
+                new String[] {"0"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new ExitAction(output)
+        };
+        new StartUI(output).init(input, tracker, actions);
+        assertThat(output.toString()).isEqualTo(
+                "Меню:" + System.lineSeparator()
+                        + "0. Завершить программу" + System.lineSeparator()
+                        + "=== Завершение программы ===" + System.lineSeparator()
+        );
     }
 }
