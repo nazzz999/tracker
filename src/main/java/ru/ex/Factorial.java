037a39c33b4ca9e9d1db2f34a01b7e31871b32af
nazarnass <nazarychevaanastacia@yandex.ru>
2025-02-09 19:58:50 +0300

    11.3 throw new RuntimeException

---
+++ src/main/java/ru/ex/Factorial.java
@@ -0,0 +1,18 @@
+package ru.ex;
+
+public class Factorial {
+    public int calc(int number) {
+        int result = 1;
+        if (number < 2) {
+            throw new IllegalArgumentException("Сообщение об ошибке");
+        }
+        for (int index = 2; index <= number; index++) {
+            result *= index;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        new Factorial().calc(-1);
+    }
+}
