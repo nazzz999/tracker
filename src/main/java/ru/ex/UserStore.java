418a9b8f2a05674e1b3659d126267abcd540c25c
nazarnass <nazarychevaanastacia@yandex.ru>
2025-02-12 12:17:01 +0300

    11.6 Иерархия исключений

---
+++ src/main/java/ru/ex/UserStore.java
@@ -0,0 +1,35 @@
+package ru.ex;
+
+public class UserStore {
+    public static User findUser(User[] users, String login) throws UserNotFoundException {
+        for (User user : users) {
+            if (user.getUsername().equals(login)) {
+                return user;
+            }
+        }
+        throw new UserNotFoundException("User with login '" + login + "' not found.");
+    }
+
+    public static boolean validate(User user) throws UserInvalidException {
+        if (!user.isValid() || user.getUsername().length() < 3) {
+            throw new UserInvalidException("The user is not valid.");
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        User[] users = {
+                new User("Ivan Ivanov", true),
+        };
+        try {
+            User user = findUser(users, "Ivan Ivanov");
+            if (validate(user)) {
+                System.out.println("This user has an access");
+            }
+        } catch (UserInvalidException e) {
+            System.out.println(e.getMessage());
+        } catch (UserNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
