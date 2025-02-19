3c7dd385c9030682f76e31c126377ecb9d635ace
nazarnass <nazarychevaanastacia@yandex.ru>
2025-02-19 13:06:36 +0300

    12.2 Singleton

--- src/main/java/ru/tracker/Tracker.java
+++ src/main/java/ru/tracker/Tracker.java
@@ -44,7 +44,7 @@ public class Tracker {
         return false;
     }
 
-    private int indexOf(int id) {
+    int indexOf(int id) {
         int rsl = -1;
         for (int index = 0; index < size; index++) {
             Item item = items[index];
