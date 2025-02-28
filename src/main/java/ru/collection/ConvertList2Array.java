3ec8928e4fa7a21bd2b725f7881bddda13f1b1cb
nazarnass <nazarychevaanastacia@yandex.ru>
2025-02-28 17:57:50 +0300

    13.6 ArrayList в двухмерный массив

---
+++ src/main/java/ru/collection/ConvertList2Array.java
@@ -0,0 +1,35 @@
+package ru.collection;
+
+import java.util.List;
+
+public class ConvertList2Array {
+    public static int[][] toArray(List<Integer> list, int cells) {
+        int groups = (int) Math.ceil((double) list.size() / cells);
+        int[][] array = new int[groups][cells];
+        int row = 0;
+        int cell = 0;
+        for (Integer number : list) {
+            array[row][cell] = number;
+            cell++;
+            if (cell >= array[row].length) {
+                cell = 0;
+                row++;
+            }
+            if (row >= array.length) {
+                break;
+            }
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = List.of(1, 2, 3, 4, 5, 6, 7);
+        int[][] result = toArray(list, 3);
+        for (int[] row : result) {
+            for (int cell : row) {
+                System.out.print(cell + " ");
+            }
+            System.out.println();
+        }
+    }
+}
