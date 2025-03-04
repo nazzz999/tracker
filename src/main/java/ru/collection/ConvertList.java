2fb28ad997b280ea19897d0f0b2847c1d4092d29
nazarnass <nazarychevaanastacia@yandex.ru>
2025-03-04 21:08:37 +0300

    13.8 Лист массивов в один лист Integer

--- src/main/java/ru/collection/ConvertList.java
+++ src/main/java/ru/collection/ConvertList.java
@@ -6,11 +6,10 @@ import java.util.List;
 public class ConvertList {
     public static List<Integer> convert(List<int[]> list) {
         List<Integer> result = new ArrayList<>();
-        for (int[] row : list) {
-            for (int cell : row) {
-                result.add(cell);
+        for (int[] array : list) {
+            for (int number : array) {
+                result.add(number);
             }
-
         }
         return result;
     }
