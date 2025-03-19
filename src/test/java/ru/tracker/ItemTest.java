0f1837afb6a3712c4fe2650b0f33cf2bc9e8ff3c
nazarnass <nazarychevaanastacia@yandex.ru>
2025-03-19 13:37:24 +0300

    17.1 Сортировка

--- src/test/java/ru/tracker/ItemTest.java
+++ src/test/java/ru/tracker/ItemTest.java
@@ -4,7 +4,6 @@ import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class ItemTest {
@@ -12,16 +11,18 @@ public class ItemTest {
     @Test
     public void testSortItemsAsc() {
         List<Item> items = new ArrayList<>();
-        items.add(new Item("Banana"));
-        items.add(new Item("Apple"));
-        items.add(new Item("Cherry"));
+        Item item = new Item("Banana");
+        Item item1 = new Item("Apple");
+        Item item2 = new Item("Cherry");
+        items.add(item);
+        items.add(item1);
+        items.add(item2);
+        items.sort(new ItemAscByName());
 
         List<Item> expected = new ArrayList<>();
-        expected.add(new Item("Apple"));
-        expected.add(new Item("Banana"));
-        expected.add(new Item("Cherry"));
-
-        Collections.sort(items, new ItemAscByName());
+        expected.add(item1);
+        expected.add(item);
+        expected.add(item2);
 
         Assertions.assertThat(items).isEqualTo(expected);
     }
@@ -29,16 +30,19 @@ public class ItemTest {
     @Test
     public void testSortItemsDesc() {
         List<Item> items = new ArrayList<>();
-        items.add(new Item("Banana"));
-        items.add(new Item("Apple"));
-        items.add(new Item("Cherry"));
+        Item item = new Item("Banana");
+        Item item1 = new Item("Apple");
+        Item item2 = new Item("Cherry");
+        items.add(item);
+        items.add(item1);
+        items.add(item2);
+
+        items.sort(new ItemDescByName());
 
         List<Item> expected = new ArrayList<>();
-        expected.add(new Item("Cherry"));
-        expected.add(new Item("Banana"));
-        expected.add(new Item("Apple"));
-
-        Collections.sort(items, new ItemDescByName());
+        expected.add(item2);
+        expected.add(item);
+        expected.add(item1);
 
         Assertions.assertThat(items).isEqualTo(expected);
     }
