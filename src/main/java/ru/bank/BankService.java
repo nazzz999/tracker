28a8e3847e569090516d8f23fb6f0ee6e0211db4
nazarnass <nazarychevaanastacia@yandex.ru>
2025-03-24 16:40:18 +0300

    18.2 Что такое JavaDoc

--- src/main/java/ru/bank/BankService.java
+++ src/main/java/ru/bank/BankService.java
@@ -8,19 +8,44 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Класс, представляющий собой сервис банка, который управляет пользователями и их аккаунтами.
+ * @author NAZARYCHEVA ANASTASIA
+ * @version 1.0
+ */
 public class BankService {
+    /**
+     * Хранение пользователей осуществляется в коллекции типа Map,
+     * где ключом является объект User, а значением список аккаунтов (Account),
+     * принаджлежащих данному пользователю.
+     */
     private final Map<User, List<Account>> users = new HashMap<>();
 
+    /**
+     * Метод принимает на вход нового пользователя и добавляет его в коллекцию.
+     * Если пользователь с таким паспортом уже существует, то он не будет добавлен.
+     * @param user пользователь который добавляется в коллекцию
+     */
     public void addUser(User user) {
         users.putIfAbsent(user, new ArrayList<>());
     }
 
+    /**
+     * Метод принимает на вход паспорт по которому нужно удалить пользователя из коллекции.
+     * @param passport паспорт пользователя, которого нужно удалить.
+     */
     public void deleteUser(String passport) {
         User user = findByPassport(passport);
         if (user != null) {
             users.remove(user);
         }
     }
+
+    /**
+     * Метод добавляет новый аккаунт для пользователя, если такой аккаунт ещё не существует.
+     * @param passport паспорт пользователя, которому добавляется аккаунт.
+     * @param account новый аккаунт который нужно добавить.
+     */
 
     public void addAccount(String passport, Account account) {
         User user = findByPassport(passport);
@@ -31,6 +56,13 @@ public class BankService {
         }
     }
 
+    /**
+     * Метод ищет пользователя по паспорту и возвращает его.
+     * Если пользователь с указанным паспортом не найден, возвращает null.
+     * @param passport паспорт пользователя, которого нужно найти
+     * @return найденный пользоваетль или null, если пользоваетль не найден.
+     */
+
     public User findByPassport(String passport) {
         for (User user : users.keySet()) {
             if (user.getPassport().equals(passport)) {
@@ -40,6 +72,14 @@ public class BankService {
         return null;
     }
 
+    /**
+     * Метод ищет аккаунт по паспорту пользователя и реквизиту аккаунта.
+     * Если аккаунт найден, возвращает его;
+     * если нет - возвращает null.
+     * @param passport паспорт пользователя, которому принадлежит аккаунт
+     * @param requisite реквизит аккаунта, который нужно найти
+     * @return найденный аккаунт или null, если аккаунт не найден
+     */
     public Account findByRequisite(String passport, String requisite) {
         User user = findByPassport(passport);
         if (user != null) {
@@ -52,6 +92,17 @@ public class BankService {
         }
         return null;
     }
+    /**
+     * Метод осуществляет перевод денег с одного аккаунта на другой.
+     * Проверяет наличие необходимых условий: существование обоих аккаунтов и достаточный баланс.
+     *
+     * @param sourcePassport паспорт источника перевода
+     * @param sourceRequisite реквизит источника перевода
+     * @param destinationPassport паспорт получателя перевода
+     * @param destinationRequisite реквизит получателя перевода
+     * @param amount сумма перевода
+     * @return true, если перевод успешен; false в противном случае
+     */
 
     public boolean transferMoney(String sourcePassport, String sourceRequisite,
                                  String destinationPassport, String destinationRequisite,
@@ -65,6 +116,11 @@ public class BankService {
         }
         return false;
     }
+    /**
+     * Метод возвращает список аккаунтов, принадлежащих указанному пользователю.
+     * @param user пользователь, для которого требуется получить список аккаунтов
+     * @return список аккаунтов данного пользователя или null, если пользователь не найден
+     */
 
     public List<Account> getAccounts(User user) {
         return users.get(user);
