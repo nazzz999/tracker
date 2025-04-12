37cfdfce235de0e722f13adb2a9f4046d8b71dd6
nazarnass <nazarychevaanastacia@yandex.ru>
2025-04-12 15:29:28 +0300

    21_7_Уникальность_элементов_и_сортировка

--- src/main/java/ru/stream/Profiles.java
+++ src/main/java/ru/stream/Profiles.java
@@ -10,4 +10,12 @@ public class Profiles {
                 .map(Profile::getAddress)
                 .collect(Collectors.toList());
     }
+
+    public static List<Address> collectSortWithoutDuplicate(List<Profile> profiles) {
+        return profiles.stream()
+                .map(Profile::getAddress)
+                .sorted((left, right) -> left.getCity().compareTo(right.getCity()))
+                .distinct()
+                .collect(Collectors.toList());
+    }
 }
