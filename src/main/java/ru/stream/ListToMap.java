a47fe1e1ab4eb78680378dfba6a2ed51a06267c3
nazarnass <nazarychevaanastacia@yandex.ru>
2025-04-14 00:38:19 +0300

    21.8 Преобразование List в Map

---
+++ src/main/java/ru/stream/ListToMap.java
@@ -0,0 +1,20 @@
+package ru.stream;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ListToMap {
+
+    public static Map<String, Student> convert(List<Student> list) {
+        return list.stream()
+                .collect(
+                        Collectors.toMap(
+                                Student::getSurname,
+                                student -> new Student(student.getScore(), student.getSurname()),
+                                (existing, replacement) -> existing
+                        )
+                );
+    }
+
+}
